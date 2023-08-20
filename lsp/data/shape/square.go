3521eb6e6f0681366a40241c386545735fbec21d
q10357 <isabel@chaparian.no>
2023-08-20 02:04:59 +0200

    added new constructor to set width and height of square

--- lsp/data/shape/square.go
+++ lsp/data/shape/square.go
@@ -1,7 +1,39 @@
 package shape
 
+/*
+Code below will work
+*/
+type Square2 struct {
+	size int
+}
+
+func (s *Square2) Rectangle() *Rectangle {
+	r := Rectangle{}
+	r.SetHeight(s.size)
+	r.SetWidth(s.size)
+	return &r
+}
+
+func (s *Square2) SetSize(size int) {
+	s.size = size
+}
+
+/*
+This square violates LSP, width and height is same, but when setting width to another
+value (or height), they may differ, resulting in wrong calculations
+*/
 type Square struct {
 	Rectangle
+}
+
+func (s *Square) SetWidth(width int) {
+	s.width = width
+	s.height = width
+}
+
+func (s *Square) SetHeight(height int) {
+	s.width = height
+	s.height = height
 }
 
 // Enforce width == height in square
--- lsp/main.go
+++ lsp/main.go
@@ -3,11 +3,19 @@ package main
 import "github.com/q10357/solid-principles-go/solid/lsp/data/shape"
 
 //Liskov Substitution Principle
+//This is a difficult subject in go, but trying to demonstrate it
 
 func main() {
 	rc := &shape.Rectangle{}
 	rc.SetHeight(2)
 	rc.SetWidth(3)
-
 	shape.UseIt(rc)
+
+	sq := shape.NewSquare(5)
+	shape.UseIt(sq)
+
+	sqRight := shape.Square2{}
+	sqRight.SetSize(10)
+	squareRect := sqRight.Rectangle()
+	shape.UseIt(squareRect)
 }
